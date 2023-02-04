6bc66c66203cfba9765f9c96423696c94f6e94d5
Arjan Kamphuis <arjan_kamphuis@hotmail.com>
2023-02-04 09:39:35 +0100

    03 - Pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+    name: { type: String, default: 'World'},
+    frameworks: Array
+});
+</script>
+
+<template>
+    <div class="w-3/4 mx-auto py-6">
+        <h1 class="text-3xl mb-3">Hello, {{ props.name }}!</h1>
+        <p>In this series, we will use the following frameworks:</p>
+        <ul>
+            <li v-for="framework in frameworks" :key="framework" class="flex items-center space-x-3">
+                <span>&#9654;</span>
+                <span v-text="framework"></span>
+            </li>
+        </ul>
+    </div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
     plugins: [
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url))
+        }
+    }
 });
