66177e927afe8bfeeba91ee440606245ed3c8643
Arjan Kamphuis <arjan_kamphuis@hotmail.com>
2023-02-04 14:16:02 +0100

    10 - Shared Data

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,15 +1,20 @@
 <script setup>
 import { computed } from 'vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, usePage } from '@inertiajs/vue3';
 import Nav from '@/Shared/Nav.vue';
+
 const title = computed(() => 'Inertia Demo');
+const user = computed(() => usePage().props.auth.user);
 </script>
 
 <template>
     <Head :title="title" />
     <section class="p-6 bg-gray-200">
         <header class="flex items-center justify-between">
-            <h1 class="font-bold text-lg" v-text="title"></h1>
+            <div class="flex items-baseline">
+                <h1 class="font-bold text-lg" v-text="title"></h1>
+                <p class="text-sm ml-4">Welcome Back, {{ user.username }}!</p>
+            </div>
             <Nav />
         </header>
     </section>
