9583f8d06eed4fe64dceb08099d91850325f0bbd
Arjan Kamphuis <arjan_kamphuis@hotmail.com>
2023-02-04 10:10:16 +0100

    05 - Progress Indicators

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 
 createInertiaApp({
+    progress: {
+        color: 'red',
+        showSpinner: true
+    },
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
         return pages[`./Pages/${name}.vue`]
